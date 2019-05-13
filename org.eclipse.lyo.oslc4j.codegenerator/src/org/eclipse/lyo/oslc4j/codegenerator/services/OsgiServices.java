001ad21ada2fb960bb761d8d286b06be64623883
Jad El-khoury <jad@kth.se>
2019-05-13 12:16:46 +0200

    closes #81
    Add a LyoDesigner version to the Application class

---
+++ org.eclipse.lyo.oslc4j.codegenerator/src/org/eclipse/lyo/oslc4j/codegenerator/services/OsgiServices.java
@@ -0,0 +1,8 @@
+package org.eclipse.lyo.oslc4j.codegenerator.services;
+
+public class OsgiServices {
+
+	public String getOSGiBundleVersion () {
+		return "getOSGiBundleVersion got called!";
+	}
+}
