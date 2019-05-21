203e022c3cd1b854d289930ace2d529549219ea1
Andrii Berezovskyi <andriib@kth.se>
2019-05-21 19:53:13 +0200

    Add return version string
    Change-Id: If72edf65c351511da99e2cfd29ce09c0619765ce
    Signed-off-by: Andrii Berezovskyi <andriib@kth.se>

--- org.eclipse.lyo.oslc4j.codegenerator/src/org/eclipse/lyo/oslc4j/codegenerator/services/OsgiServices.java
+++ org.eclipse.lyo.oslc4j.codegenerator/src/org/eclipse/lyo/oslc4j/codegenerator/services/OsgiServices.java
@@ -1,8 +1,12 @@
 package org.eclipse.lyo.oslc4j.codegenerator.services;
 
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.Version;
+
 public class OsgiServices {
 
-	public String getOSGiBundleVersion () {
-		return "getOSGiBundleVersion got called!";
+	public String getOSGiBundleVersion() {
+		Version version = FrameworkUtil.getBundle(getClass()).getVersion();
+		return version.toString();
 	}
 }
