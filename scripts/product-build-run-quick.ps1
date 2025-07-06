0b2bdcf543d4b68a1b5afc745674e750828c8870
Andrew Berezovskyi <andrew@berezovskyi.me>
2025-07-06 20:14:48 +0200

    fix: ignore all imports
    due to https://github.com/diffplug/spotless/issues/2042

--- scripts/product-build-run-quick.ps1
+++ scripts/product-build-run-quick.ps1
@@ -3,13 +3,18 @@ $LyoDesignerRepoRoot = "$PSScriptRoot\.."
 
 Push-Location $LyoDesignerRepoRoot
 try {
-    mvn clean install -pl org.eclipse.lyo.tools.codegenerator.feature -am; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    #    Set-Location $LyoDesignerRepoRoot\org.eclipse.lyo.oslc4j.codegenerator
+    mvn install -pl org.eclipse.lyo.oslc4j.codegenerator -am; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    Set-Location $LyoDesignerRepoRoot\org.eclipse.lyo.tools.codegenerator.feature
+    mvn install; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
     Set-Location $LyoDesignerRepoRoot\org.eclipse.lyo.tools.designer.product
-    mvn clean install; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+    mvn install; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
     Set-Location $LyoDesignerRepoRoot\org.eclipse.lyo.tools.designer.product\target\products
-    & "$LyoDesignerRepoRoot\org.eclipse.lyo.tools.designer.product\org.eclipse.lyo.tools.designer.product\target\products\lyo-designer\win32\win32\x86_64\lyodesigner.exe"; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+    & "$LyoDesignerRepoRoot\org.eclipse.lyo.tools.designer.product\target\products\lyo-designer\win32\win32\x86_64\lyodesigner.exe"; if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 } catch {
     exit 1
 } finally {
